6ee29f0b3d584b86e5a475aa8693cd7a409b869e
antonionoronha <antonio.noronhaneto@gmail.com>
2020-10-05 21:02:50 -0300

    commit

---
+++ build/precache-manifest.a91f40c0263d7004f50f29611585242c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf4b9abf649ec1c8ca98961a7e0c19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3c36da69ac65c5ae56",
+    "url": "/static/css/main.837cb20a.chunk.css"
+  },
+  {
+    "revision": "c2aa02d2ef051a7ee045",
+    "url": "/static/js/2.4c7f41ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c7f41ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3c36da69ac65c5ae56",
+    "url": "/static/js/main.527a2737.chunk.js"
+  },
+  {
+    "revision": "ff2a096daed7f060ed07",
+    "url": "/static/js/runtime-main.270d71e5.js"
+  }
+]);
